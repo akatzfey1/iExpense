439d22445c2165557b9f02b576ccb4e3fe1e00d3
akatzfey <katzfeyalex@gmail.com>
2022-03-08 17:29:01 -0800

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Alexander Katzfey on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
